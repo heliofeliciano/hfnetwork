0f4230ac69a06e3188e5039e6d4e7016f125e4b6
Helio Feliciano <heliofeliciano@gmail.com>
2023-02-14 15:23:06 -0300

    add example files

---
+++ Example/Pods/Target Support Files/Pods-HFNetwork_Example/Pods-HFNetwork_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HFNetwork_Example : NSObject
+@end
+@implementation PodsDummy_Pods_HFNetwork_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-HFNetwork_Tests/Pods-HFNetwork_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HFNetwork_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_HFNetwork_Tests
+@end
